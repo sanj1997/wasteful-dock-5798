6329d6234374017aa3c1c458c9751983544599ba
Js Sanjay <mname8741@gmail.com>
2022-11-09 19:28:30 +0530

    routing

--- backend/features/models/user.model.js
+++ backend/features/models/user.model.js
@@ -5,13 +5,14 @@ const UserSchema=new Schema(
         firstName:{type:String,required:true},
         lastName:{type:String,required:true},
         userName:{type:String,required:true,unique:true},
-        userAvatar:{type:URL},
+        userAvatar:{type:String},
         email:{type:String,unique:true},
         phone:{type:Number,unique:true},
         password:{type:String,required:true},
         address:{type:Object},
         gender:{type:String,enum:["Male","Female","Other"]},
-        role:{type:String,enum:["Admin","Seller","Customer"],default:"Customer"}
+        role:{type:String,enum:["Admin","Seller","Customer"],default:"Customer"},
+        isVerified:{type:Boolean,default:false}
     },{timestamps:true}
 )
 
