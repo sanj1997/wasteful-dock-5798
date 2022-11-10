11b5f44db3f19764c3823440c897f60f39a0356e
sumangaldey8972 <sumangaldey8972@gmail.com>
2022-11-11 00:39:58 +0530

    final commit for day3 merge resolved conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,6 @@ import Navbar from "./components/Navbar";
 import AdminDashboard from "./pages/AdminDashboard";
 import CartModal from "./components/CartModal";
 import Address from "./pages/Address";
-import Login from "./pages/Login";
-import Otpverify from "./pages/Otpverify";
-import AddUser from "./pages/AddUser";
 import Home from "./pages/Home";
 
 
