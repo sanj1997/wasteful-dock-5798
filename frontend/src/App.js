e9e977c33ba376aaea5df31474215535e2f550e6
Gautam Kumarm <g2gautamsingh@gmail.com>
2022-11-10 16:14:01 +0530

    pament sidebar section added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 
 import CartModal from "./components/CartModal";
 import Address from "./pages/Address";
+import Payment from "./pages/Payment";
 
 function App() {
   return (
     <div>
       {/* <CartModal /> */}
-     <Address/>
+      {/* <Address/> */}
+      <Payment />
     </div>
   );
 }
