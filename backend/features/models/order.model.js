efa9dbdc4a037a58726abd8031a53f72955a61eb
sumangaldey8972 <sumangaldey8972@gmail.com>
2022-11-11 23:08:55 +0530

    home page done

--- backend/features/models/order.model.js
+++ backend/features/models/order.model.js
@@ -1,19 +1,24 @@
-const {Schema,model}=require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const OrderSchema=new Schema(
-    {
-        userId:{type:Schema.Types.ObjectId,ref:'user'},
-        products:[
-            {
-                productId:{type:Schema.Types.ObjectId,ref:'product'},
-                quantity:{type:Number,default:1}
-            }
-        ],
-        amount:{type:Number,required:true},
-        address:{type:Schema.Types.ObjectId,ref:'user'},
-        status:{type:String,enum:['Pending','Successful'],default:'Pending'}
-    },{timestamps:true}
-)
+const OrderSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "user" },
+    products: [
+      {
+        productId: { type: Schema.Types.ObjectId, ref: "product" },
+        quantity: { type: Number, default: 1 },
+      },
+    ],
+    amount: { type: Number, required: true },
+    address: { type: Schema.Types.ObjectId, ref: "user" },
+    status: {
+      type: String,
+      enum: ["Pending", "Successful"],
+      default: "Pending",
+    },
+  },
+  { timestamps: true }
+);
 
-const OrderModel=model('order',OrderSchema);
-module.exports=OrderModel;
+const OrderModel = model("order", OrderSchema);
+module.exports = OrderModel;
