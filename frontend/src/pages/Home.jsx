1b545a587466994fb160b96318847fcc6275a598
Gautam Kumarm <g2gautamsingh@gmail.com>
2022-11-12 12:06:27 +0530

    home page delete

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,1 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
-import Footer from '../components/Footer/Footer'
-import Navbar from '../components/Navbar/Navbar'
 
-
-const Home = () => {
-  return (
-    <div>
-      <Navbar />
-      <Footer />
-    </div>
-  )
-}
-
-export default Home
